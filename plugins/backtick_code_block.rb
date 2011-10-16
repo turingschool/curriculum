3714f2d9fcc8473faaa1bb8d302e3020d31f7612
Frederic Hemberger <mail@frederic-hemberger.de>
2011-10-16 12:02:14 +0200

    Removes also leading tab from backtick codeblock

--- plugins/backtick_code_block.rb
+++ plugins/backtick_code_block.rb
@@ -10,41 +10,34 @@ module BacktickCodeBlock
     @lang = nil
     @url = nil
     @title = nil
-    begin
-      input.gsub /^(\s*)`{3} *([^\n]+)?\n(.+?)\n\s*`{3}/m do
-        indentation = $1.delete("\n")
-        @options = $2 || ''
-        str = $3
+    input.gsub(/^`{3} *([^\n]+)?\n(.+?)\n`{3}/m) do
+      @options = $1 || ''
+      str = $2
 
-        if @options =~ AllOptions
-          @lang = $1
-          @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
-        elsif @options =~ LangCaption
-          @lang = $1
-          @caption = "<figcaption><span>#{$2}</span></figcaption>"
-        end
-
-        if str.match(/\A {4}/)
-          str = str.gsub /^ {4}/, ''
-        end
-      
-        figure = if @lang.nil? || @lang == 'plain'
-          code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
-          "<figure class='code'>#{@caption}#{code}</figure>"
-        else
-          if @lang.include? "-raw"
-            raw = "``` #{@options.sub('-raw', '')}\n"
-            raw += str
-            raw += "\n```\n"
-          else
-            code = highlight(str, @lang)
-            "<figure class='code'>#{@caption}#{code}</figure>"
-          end
-        end
-        "\n" + indentation + figure
+      if @options =~ AllOptions
+        @lang = $1
+        @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
+      elsif @options =~ LangCaption
+        @lang = $1
+        @caption = "<figcaption><span>#{$2}</span></figcaption>"
       end
-    rescue
-      raise "Exception on code sample: #{input[0..150]}"
+
+      if str.match(/\A( {4}|\t)/)
+        str = str.gsub(/^( {4}|\t)/, '')
+      end
+      if @lang.nil? || @lang == 'plain'
+        code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
+        "<figure class='code'>#{@caption}#{code}</figure>"
+      else
+        if @lang.include? "-raw"
+          raw = "``` #{@options.sub('-raw', '')}\n"
+          raw += str
+          raw += "\n```\n"
+        else
+          code = highlight(str, @lang)
+          "<figure class='code'>#{@caption}#{code}</figure>"
+        end
+      end
     end
   end
 end

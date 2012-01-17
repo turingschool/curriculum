7f2281a4917de302f22326d630d8e8944f7e4476
Jeff Casimir <jeff@casimircreative.com>
2012-01-17 17:14:49 -0500

    Debugging for Heroku

--- plugins/backtick_code_block.rb
+++ plugins/backtick_code_block.rb
@@ -10,37 +10,41 @@ module BacktickCodeBlock
     @lang = nil
     @url = nil
     @title = nil
-    input.gsub /^(\s*)`{3} *([^\n]+)?\n(.+?)\n\s*`{3}/m do
-      indentation = $1.delete("\n")
-      @options = $2 || ''
-      str = $3
+    begin
+      input.gsub /^(\s*)`{3} *([^\n]+)?\n(.+?)\n\s*`{3}/m do
+        indentation = $1.delete("\n")
+        @options = $2 || ''
+        str = $3
 
-      if @options =~ AllOptions
-        @lang = $1
-        @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
-      elsif @options =~ LangCaption
-        @lang = $1
-        @caption = "<figcaption><span>#{$2}</span></figcaption>"
-      end
-
-      if str.match(/\A {4}/)
-        str = str.gsub /^ {4}/, ''
-      end
-      
-      figure = if @lang.nil? || @lang == 'plain'
-        code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
-        "<figure class='code'>#{@caption}#{code}</figure>"
-      else
-        if @lang.include? "-raw"
-          raw = "``` #{@options.sub('-raw', '')}\n"
-          raw += str
-          raw += "\n```\n"
-        else
-          code = highlight(str, @lang)
-          "<figure class='code'>#{@caption}#{code}</figure>"
+        if @options =~ AllOptions
+          @lang = $1
+          @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
+        elsif @options =~ LangCaption
+          @lang = $1
+          @caption = "<figcaption><span>#{$2}</span></figcaption>"
         end
+
+        if str.match(/\A {4}/)
+          str = str.gsub /^ {4}/, ''
+        end
+      
+        figure = if @lang.nil? || @lang == 'plain'
+          code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
+          "<figure class='code'>#{@caption}#{code}</figure>"
+        else
+          if @lang.include? "-raw"
+            raw = "``` #{@options.sub('-raw', '')}\n"
+            raw += str
+            raw += "\n```\n"
+          else
+            code = highlight(str, @lang)
+            "<figure class='code'>#{@caption}#{code}</figure>"
+          end
+        end
+        "\n" + indentation + figure
       end
-      "\n" + indentation + figure
+    rescue
+      raise "Exception on code sample: #{input[0..150]}"
     end
   end
 end

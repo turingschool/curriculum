de17b11bd869ed9b2bc3e440fb65b55de6f9d9d1
Jeff Casimir <jeff@jumpstartlab.com>
2012-06-27 21:48:56 -0400

    Dumping jQuery content into repo

---
+++ source/projects/jquery/JSCart/data.js
@@ -0,0 +1,7 @@
+var raw_inventory = [
+  {"price":4.99,"name":"Monkey","stock":5,"product_id":1},
+  {"price":4.99,"name":"Dog","stock":4,"product_id":2},
+  {"price":6.99,"name":"Cat","stock":3,"product_id":3},
+  {"price":5.99,"name":"Sword","stock":2,"product_id":4},
+  {"price":7.99,"name":"Hat","stock":1,"product_id":5}
+]

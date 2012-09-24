28e7463ad1e1ad26ddbdab2c68a81a009ff4da70
Franklin Webber <franklin.webber@gmail.com>
2012-09-24 14:12:02 -0400

    TOC following and collapsable all the time

--- source/javascripts/sidebar.js
+++ source/javascripts/sidebar.js
@@ -1,17 +1,17 @@
 $(function() {
 
   var JSLSidebar = function() { 
-    
+
     $("#content").css('margin-right','240px');
     $("#content div:first").css('border-right','1px solid #E0E0E0');
-    
-    function _addToggler() {
+
+    function addToggler() {
       $('.toggle-sidebarn').bind('click', function(e) {
         e.preventDefault();
         if ($('body').hasClass('collapse-sidebarn')) {
           $("#content").css('margin-right','240px');
           $('body').removeClass('collapse-sidebarn');
-          
+
         } else {
           $("#content").css('margin-right','20px');
           $('body').addClass('collapse-sidebarn');
@@ -30,39 +30,72 @@ $(function() {
       }
       if (sections.length >= 3){ $('aside.sidebarn').addClass('thirds'); }
     }
-    
-    function _followingContent() {
-      
-      var topSection = $("aside.sidebarn div:first");
-  
-      var topPosition = topSection.offset().top - parseFloat(topSection.css('margin-top').replace(/auto/, 0));
-  
-      $(window).scroll(function(event) {
-        var y = $(this).scrollTop();
 
-        if (y >= topPosition) {
-          topSection.addClass('fixed');
-      
-          if (topSection.height() > $(this).height()) {
-            var contentDifference = $(this).height() - topSection.height() + (y - topPosition)
-            if (contentDifference > 40) { contentDifference = 20; }
-      
-            topSection.css('bottom',contentDifference)
-          } else {
-            topSection.css('top','20')
-          }
-      
+    function positionContentWithinWindow(topSection,topPosition) {
+
+      var y = $(window).scrollTop();
+
+      if (y >= topPosition) {
+        topSection.addClass('fixed');
+
+        if (topSection.height() > $(window).height()) {
+          var contentDifference = $(window).height() - topSection.height() + (y - topPosition);
+          if (contentDifference > 40) { contentDifference = 20; }
+          topSection.css('bottom',contentDifference);
         } else {
-          topSection.removeClass('fixed')
+          topSection.css('bottom','');
+          topSection.css('top','20');
         }
-        
-      });
-      
+
+      } else {
+        topSection.removeClass('fixed');
+      }
+
     }
-  
-    _addToggler();
-    _followingContent();
     
+    function enableContentToFollowWithWindowScrollAndResize() {
+
+      var topSection = $("aside.sidebarn div:first");
+      var topPosition = topSection.offset().top - parseFloat(topSection.css('margin-top').replace(/auto/, 0));
+
+      $(window).scroll(function(event) {
+        positionContentWithinWindow(topSection,topPosition);
+      });
+
+      $(window).resize(function(event) {
+        positionContentWithinWindow(topSection,topPosition);
+      });
+
+    }
+
+    function populateWithContent() {
+      generateLinksForContent("aside.sidebarn div:first","article header");
+      generateLinksForContent("aside.sidebarn div:first","#content article h2");
+    }
+
+    function generateLinksForContent(targetSelector,contentSelector) {
+
+      var contentItemTarget = $(targetSelector);
+
+      $(contentSelector).each(function(index,headerItem) {
+
+        var contentItem = $(headerItem);
+        var contentItemSlug = contentItem.text().trim().toLowerCase().replace(/\s/g,'-');
+
+        var anchorName = "<a name='" + contentItemSlug + "' />";
+        $(anchorName).insertBefore(contentItem);
+
+        var contentAnchorLink = "<section><h3><a href='#" + contentItemSlug + "'>" + contentItem.text().trim() + "</h3></a></section>";
+
+        contentItemTarget.append(contentAnchorLink);
+
+      });
+
+    }
+
+    addToggler();
+    enableContentToFollowWithWindowScrollAndResize();
+    populateWithContent();
   }
 
   var sidebar = new JSLSidebar();

9e60ec18aeb7f64adc1fe16b8d8527aeb844e505
Franklin Webber <franklin.webber@gmail.com>
2012-09-25 08:27:31 -0400

    Aliased window tag and cleaned up previous, out-of-date docs

--- plugins/window_tag.rb
+++ plugins/window_tag.rb
@@ -1,18 +1,30 @@
-# Title: Simple Video tag for Jekyll
-# Author: Brandon Mathis http://brandonmathis.com
-# Description: Easily output MPEG4 HTML5 video with a flash backup.
 #
-# Syntax {% video url/to/video [width height] [url/to/poster] %}
+# Generates a Mac OS X window around the specified content.
 #
 # Example:
-# {% video http://site.com/video.mp4 720 480 http://site.com/poster-frame.jpg %}
+# {% terminal %}
+# $ gem install rails
+# {% endterminal %}
+#
+# {% window terminal %}
+# $ gem install rails
+# {% endwindow %}
 #
 # Output:
-# <video width='720' height='480' preload='none' controls poster='http://site.com/poster-frame.jpg'>
-#   <source src='http://site.com/video.mp4' type='video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"'/>
-# </video>
+# <div class="window">
+#   <nav class="control-window">
+#   <a href="#finder" class="close" data-rel="close">close</a>
+#   <a href="#" class="minimize">minimize</a>
+#   <a href="#" class="deactivate">deactivate</a>
+#   </nav>
+#   <h1 class="titleInside">Terminal</h1>
+#   <div class="container">
+#     <div class="terminal">
+#     $ gem install sunlight
+#     </div>
+#   </div>
+# </div>
 #
-
 module Jekyll
 
   class WindowTag < Liquid::Block
@@ -41,6 +53,5 @@ module Jekyll
 
 end
 
-
-
+Liquid::Template.register_tag('terminal', Jekyll::WindowTag)
 Liquid::Template.register_tag('window', Jekyll::WindowTag)

cf3e2d26b810b4bf41760943a12c2019fc799fa5
Franklin Webber <franklin.webber@gmail.com>
2012-10-02 20:38:41 -0400

    FIX: PDF generation will now use the source stylesheets

--- plugins/pdf_generator.rb
+++ plugins/pdf_generator.rb
@@ -8,13 +8,13 @@ module Jekyll
     def post_write(content)
       generate_pdf(content) if content.data['pdf']
     end
-    
+
     def generate_pdf(content)
       filepath = "public#{content.dir}#{content.url}"
       pdf_filepath = "#{filepath[0..-5]}pdf"
       puts "Converting #{filepath} into #{pdf_filepath}"
       pdf = PDFKit.new content.content, "print-media-type" => true
-      pdf.stylesheets << "public/stylesheets/screen.css"
+      pdf.stylesheets << "source/stylesheets/screen.css"
       pdf.to_file pdf_filepath
     end
 

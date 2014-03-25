a3d99e632ac715aa2bf8baead075aa46204b298c
Mike Chlipala <mike@chlipala.com>
2014-03-25 16:40:19 -0700

    Use Swiftype for search

--- source/search.js
+++ source/search.js
@@ -1,41 +1,12 @@
 ---
 ---
-var docs =
-[
-{% for page in site.pages %}
-  {% include post.json %},
-{% endfor %}
-];
-// init lunr
-var idx = lunr(function () {
-  this.field('title', 10);
-  this.field('content');
-})
-// add each document to be index
-for(var index in docs) {
-  idx.add(docs[index]);
-}
+var Swiftype = window.Swiftype || {};
+  (function() {
+    Swiftype.key = 'zLf7v5eGviG7iqxpQYXm';
 
-$(function() {
-  $("#search button").click(function() {
-    search();
-  });
-
-  $("#search input").keypress(function(e) {
-    if(e.which == 13) {
-      e.preventDefault();
-      search();
-    }
-  });
-})
-
-function search() {
-  var result = idx.search($("#search input").val());
-  if(result && result.length > 0) {
-    window.location.replace(result[0].ref);
-  } else {
-    if($(".no-results").length === 0){
-      $(".main-navigation").append("<li class='no-results'>" + "No results found" + "<li>");
-    }
-  }
-}
+    /** DO NOT EDIT BELOW THIS LINE **/
+    var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
+    script.src = "//s.swiftypecdn.com/embed.js";
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+  }());

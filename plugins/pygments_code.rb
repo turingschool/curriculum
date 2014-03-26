229773ddd36ab97691a09a2ea87128571136bb90
Jeff Casimir <jeff@casimircreative.com>
2014-03-26 12:12:48 -0700

    Hack until I have time to figure out modifying the buildpack

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -13,8 +13,9 @@ module HighlightCode
   end
 
   def connect_to_highlight_store
-    puts "Build arguments: #{ARGV.inspect}"
-    host = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+    #puts "Build arguments: #{ARGV.inspect}"
+    #host = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+    host = "redis://redistogo:efe730154afe8faa640bd370192469d1@albacore.redistogo.com:9352/"
     uri = URI.parse(host)
     store = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     puts "Connecting to highlight store #{store.inspect}"

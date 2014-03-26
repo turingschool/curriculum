c4bb347211ad291889329bf30f08d09215606c9f
Jeff Casimir <jeff@casimircreative.com>
2014-03-26 12:53:49 -0700

    Triggering rebuild

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -13,6 +13,7 @@ module HighlightCode
   end
 
   def connect_to_highlight_store
+    
     #host = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
     host = "redis://redistogo:efe730154afe8faa640bd370192469d1@albacore.redistogo.com:9352/"
     uri = URI.parse(host)

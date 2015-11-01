f25a56cf3e6df5ee54f25dc7812edb1b47808619
Jeff Casimir <jeff@casimircreative.com>
2015-11-01 16:53:50 -0700

    Working draft of the HTTP sample code

---
+++ source/projects/http_yeah_you_know_me.rb
@@ -0,0 +1,28 @@
+require 'socket'
+tcp_server = TCPServer.new(9292)
+client = tcp_server.accept
+
+puts "Ready for a request"
+
+request_lines = []
+while line = client.gets and !line.chomp.empty?
+  request_lines << line.chomp
+end
+
+puts "Got this request:"
+puts request_lines.inspect
+
+puts "Sending response."
+response = "<pre>" + request_lines.join("\n") + "</pre>"
+output = "<html><head></head><body>#{response}</body></html>"
+
+headers = ["http/1.1 200 ok",
+          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+          "server: ruby",
+          "content-type: text/html; charset=iso-8859-1",
+          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+client.puts headers
+client.puts output
+puts "\nWrote this response:\n#{output}"
+client.close
+puts "\nResponse complete, exiting."
